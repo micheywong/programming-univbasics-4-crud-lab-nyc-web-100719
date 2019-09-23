d57a27061074ecd1041d28a5519a9885d21e49a9
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:13:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_family = ["Susie", "Henry", "Michael", "Lauren"]
+    array = ["wow", "I", "am", "really", "learning"]
   my_family << "Michelle"
 end
 
