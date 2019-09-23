f6e26b275ff07e53f149dd157debb444483ee3ee
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:05:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_family = ["Susie", "Henry"]
+  my_family = ["Susie", "Henry", "Michael", "Lauren"]
 end
 
 def add_element_to_end_of_array(array, element)
