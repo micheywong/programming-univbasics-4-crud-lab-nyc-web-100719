607f03f743b49417ad9ee1a83703c78e3b1b137a
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:07:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_family = ["Susie", "Henry", "Michael", "Lauren"]
-  
+  michael_wong = my_family.pop
 end
 
 def remove_element_from_start_of_array(array)
