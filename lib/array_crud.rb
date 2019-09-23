4d31e540b11219542e3340e2c5d1cc921c093f50
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:17:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  my_family = ["Susie", "Henry", "Michael", "Lauren"]
-  susie_wong = my_family.shift
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
