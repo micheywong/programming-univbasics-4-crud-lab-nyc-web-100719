013ba600c18abcd962d77794e8a0523f04a05f17
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:12:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_family = ["Susie", "Henry", "Michael", "Lauren"]
-  my_family << "Michelle"
+  my_family<<"Michelle"
 end
 
 def add_element_to_start_of_array(array, element)
