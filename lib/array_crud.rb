a790c6e4c813d20059cfdf7d5d8fb51d40038cbf
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 13:09:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_family = ["Susie", "Henry", "Michael", "Lauren"]
-  my_family[0]
+  my_family[2]
 end
 
 def retrieve_first_element_from_array(array)
